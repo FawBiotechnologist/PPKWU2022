e745e14efcdfc1defd24da92a34ef650f284b14a
Faw <66173126+FawBiotechnologist@users.noreply.github.com>
2022-10-20 13:40:39 +0200

    bug fixing

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -2,47 +2,51 @@
 import http.server
 import socketserver
 import os
+from datetime import datetime
 
-#print('source code for "http.server":', http.server.__file__)
+
+# print('source code for "http.server":', http.server.__file__)
 
 class web_server(http.server.SimpleHTTPRequestHandler):
-    
+
     def do_GET(self):
 
         print(self.path)
-        
+
         if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()   
-            msgToBePrinted = "Hello World!"         
-            if rev:
-            	msgToBePrinted = rev(txt)	           	
+            self.end_headers()
+            msgToBePrinted = "Hello World!"
             msgToBePrinted = msgToBePrinted + "\n"
-            self.wfile.write(msgToBePrinted.encode(encoding = 'UTF-8'))
- 	elif self.path.startswith('/cmd'):
+            self.wfile.write(msgToBePrinted.encode(encoding='UTF-8'))
+        elif self.path.startswith('/cmd'):
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()   
+            self.end_headers()
             if self.path.split('=')[1] == 'time':
-            	msgToBePrinted = datetime.now().strftime("%H:%M:%S")
+                msgToBePrinted = datetime.now().strftime("%H:%M:%S")
             elif self.path.split('=')[1] == 'rev&str':
-            	msgToBePrinted = rev(self.path.split('=')[2])
+                msgToBePrinted = self.rev(self.path.split('=')[2])
+            else:
+                msgToBePrinted = ""
             msgToBePrinted = msgToBePrinted + "\n"
-            self.wfile.write(msgToBePrinted.encode(encoding = 'UTF-8'))
+            self.wfile.write(msgToBePrinted.encode(encoding='UTF-8'))
         else:
             super().do_GET()
-            
-     def rev(x):
-  	return x[::-1]
-    
+
+    def rev(self, x):
+        return x[::-1]
+
+
 # --- main ---
 
 PORT = 4080
 
 print(f'Starting: http://localhost:{PORT}')
 
-tcp_server = socketserver.TCPServer(("",PORT), web_server)
+tcp_server = socketserver.TCPServer(("", PORT), web_server)
 tcp_server.serve_forever()
+
