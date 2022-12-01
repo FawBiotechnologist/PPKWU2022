072466ac320e30dd74c48a8016448b92cb418d59
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-12-01 14:16:10 +0100

    initial commit

---
+++ lab5/source/server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from flask import Flask, jsonify
+import re
+from flask import request
+import json
+
+
+def calculate(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return {"sum": num1 + num2, "sub": num1 - num2, "mul": num1 * num2, "div": num1 // num2, "mod": num1 % num2}
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def get_numbers():
+    return calculate(request.args.get('num1'), request.args.get('num2'))
+
+
+# --- main ---
+if __name__ == '__main__':
+    PORT = 4080
+
+    print(f'Starting: http://localhost:{PORT}')
+    app.run(debug=False, host='0.0.0.0', port=PORT)
