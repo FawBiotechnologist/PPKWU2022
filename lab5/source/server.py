bcbda864574607d5576e09df054e2078b4e0902e
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-12-01 14:29:34 +0100

    trying to solve an issue

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -10,6 +10,17 @@ def calculate(num1, num2):
     num2 = int(num2)
     return {"sum": num1 + num2, "sub": num1 - num2, "mul": num1 * num2, "div": num1 // num2, "mod": num1 % num2}
 
+def statistics(string):
+    lowercase = len(re.findall(r'[a-z]', string))
+    uppercase = len(re.findall(r'[A-Z]', string))
+    digits = len(re.findall(r'[1-9]', string))
+    special = len(string) - lowercase - uppercase - digits
+    return {
+        "lowercase": lowercase,
+        "uppercase": uppercase,
+        "digits": digits,
+        "special": special
+    }
 
 app = Flask(__name__)
 
@@ -17,6 +28,9 @@ app = Flask(__name__)
 @app.route("/", methods=['POST'])
 def get_numbers():
     request_json = request.get_json()
+    string = request_json.get("str")
+    num1 = request_json.get("num1")
+    num2 = request_json.get("num2")
     return calculate(request.args.get('num1'), request.args.get('num2'))
 
 
